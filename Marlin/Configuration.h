31381eec983f0a259c0fb00a9d899586c039d15b
Eragon3650 <eragon3650@gmail.com>
2023-04-03 20:07:01 -0400

    adjust bed offset for micro swiss

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1700,8 +1700,8 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS (-4)
-#define Y_MIN_POS 0
+#define X_MIN_POS (-5)
+#define Y_MIN_POS (-10)
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE + X_MIN_POS)
 #define Y_MAX_POS (Y_BED_SIZE + Y_MIN_POS)
